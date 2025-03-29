ab3f2724535f0d7d5da826e0a501e01e789665d1
1965508252 <1965508252@qq.com>
2025-03-29 16:49:21 +0800

    feat: 实现交替合并两个字符串的功能
    - 新增 mergeAlternately 函数，用于将两个字符串交替合并
    - 函数参数为两个字符串 word1 和 word2
    - 实现了内存分配、交替添加字符以及处理剩余字符的逻辑
    - 返回合并后的字符串

---
+++ main-mergeAlternately.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+char* mergeAlternately(char* word1, char* word2) {
+    // 获取两个字符的长度
+    int leth1 = strlen(word1);
+    int leth2 = strlen(word2);
+
+    // 为定义的返回值分配内存
+    char* resStr = (char*)malloc((leth1 + leth2 + 1)*sizeof(char));
+
+    // 判断内存分配是否成功
+    if(resStr == NULL){
+        return NULL;
+    };
+
+    // 定义三个值用于循环，及存储新数组
+    int i= 0,j=0,k=0;
+
+    // 交替添加字符
+    while(i<leth1 && j<leth2){
+        resStr[k++] = word1[i++];
+        resStr[k++] = word2[j++];
+    };
+
+    // 判断两个字符组有没有剩余内容
+    while(i<leth1){
+        resStr[k++] = word1[i++];
+    };
+
+    while(j<leth2){
+        resStr[k++] = word2[j++];
+    }
+    resStr[k] = '\0';
+    return resStr;
+}

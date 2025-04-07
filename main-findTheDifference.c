0bd7396e2029fd63fe2289559e3dfb8b50435dd8
1965508252 <1965508252@qq.com>
2025-04-07 09:15:50 +0800

    feat(main): 替换字符串合并功能为字符差异查找功能

--- main-findTheDifference.c
+++ main-findTheDifference.c
@@ -2,22 +2,6 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main()
-{
-    setlocale(LC_ALL, "zh_CN.UTF-8");
-
-    char *strs1 = "hello";
-    char *strs2 = "world";
-
-    char *resStrs = getMergeStrs(strs1, strs2);
-
-    printf("%s\n", resStrs);
-    free(resStrs);
-
-    system("pause");
-    return 0;
-}
-
 char *findTheDifference(char *s, char *t)
 {
     int count[26] = {0};
